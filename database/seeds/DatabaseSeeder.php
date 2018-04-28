ad27a41ee265932fc5428a9d8a1941e891373af0
issamYahiaoui <ei_yahiaoui@esi.dz>
2018-04-28 05:23:38 +0200

    seed users with location

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -48,13 +48,54 @@ class DatabaseSeeder extends Seeder
 
         for ($i = 0; $i < 10; $i++) {
             \App\User::create([
-                'name' => 'resucer_'.$i,
-                'email' => $i.'resucer@resucer.com',
+                'name' => 'resucer_' . $i,
+                'email' => $i . 'resucer@resucer.com',
                 'password' => bcrypt('123456'),
-                'lng' => $i*2.4,
-                'lat' => $i*2.7,
+                'lng' => $i * 2.4,
+                'lat' => $i * 2.7,
                 'active' => 1
             ]);
         }
+        \App\User::create([
+            'name' => 'oussama',
+            'email' => 'resucer@resucer.com',
+            'password' => bcrypt('123456'),
+            'lng' => 3.1744519478341197,
+            'lat' => 36.70443880839400,
+            'active' => 1
+        ]);
+        \App\User::create([
+            'name' => 'issam',
+            'email' => 'resucer14@resucer.com',
+            'password' => bcrypt('123456'),
+            'lng' => 3.1744519478341197,
+            'lat' => 36.70443880839400,
+            'active' => 1
+        ]);
+        \App\User::create([
+            'name' => 'alaeddine',
+            'email' => 'resucer11@resucer.com',
+            'password' => bcrypt('123456'),
+            'lng' => 3.1754519478341197,
+            'lat' => 36.70443880839400,
+            'active' => 1
+        ]);
+        \App\User::create([
+            'name' => 'mohamed',
+            'email' => 'resucer12@resucer.com',
+            'password' => bcrypt('123456'),
+            'lng' => 3.1654519478341197,
+            'lat' => 36.70443880839400,
+            'active' => 1
+        ]);
+
+        \App\User::create([
+            'name' => 'younes',
+            'email' => 'resucer13@resucer.com',
+            'password' => bcrypt('123456'),
+            'lng' => 3.1754519478341197,
+            'lat' => 36.70243880839400,
+            'active' => 1
+        ]);
+
     }
-}
